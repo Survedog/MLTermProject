718e669584be55920e649b9becbac85fd8b82fd5
survedog <survedog@gmail.com>
2023-06-16 01:38:25 +0900

    Clustering & PCA in Model.py

--- Model.py
+++ Model.py
@@ -27,33 +27,91 @@ subject_metadata = pd.read_csv('C:/Users/INHA/Documents/MLTermProject/data/metad
 # print(student_metadata)
 # print(subject_metadata)
 
-# [Calculate features for measuring quality]
+# [Using Clustering, Divide students by their academic achievement] *
 answer_integrated = pd.merge(answer_correct_data, answer_metadata, 'inner', 'AnswerId')
-answer_integrated_group = answer_integrated.groupby('QuestionId')
-train_data = pd.DataFrame(columns=['CorrectRate', 'MeanConfidence', 'AnswerVariance'])
+answer_integrated_user_group = answer_integrated.groupby('UserId')
+student_solve_info = pd.DataFrame(columns=['SolvedCount', 'CorrectRate', 'MeanConfidence'])
+student_solve_info[['CorrectRate', 'MeanConfidence']] = answer_integrated_user_group.mean()[['IsCorrect', 'Confidence']]
+student_solve_info['SolvedCount'] = answer_integrated_user_group.size()
+student_solve_info.reset_index(inplace=True)
+
+# Exclude those who solved less than 50 questions.
+student_solve_info = student_solve_info[student_solve_info['SolvedCount'] >= 50]
+
+# # Check if solvedCount is correctly computed.
+# for index, row_content in student_solve_info.iterrows():
+#     user_id = row_content[0]
+#     computed_solved_count = row_content[1]
+#     solved_count = len(answer_correct_data[answer_correct_data['UserId'] == user_id])
+#     if computed_solved_count != solved_count:
+#         print('SolvedCount computed incorrectly.')
+
+# Exclude rows with NaN value. (For clustering)
+student_solve_info.isnull().sum() # 634 rows with NaN
+student_solve_info.dropna(axis='index', inplace=True)
+
+# Perform feature scaling for student info features before clustering.
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+student_solve_info_scaled = scaler.fit_transform(student_solve_info, y=None)
+student_solve_info_scaled = pd.DataFrame(student_solve_info_scaled, index=student_solve_info.index, columns=student_solve_info.columns)
+student_solve_info_scaled[['UserId', 'SolvedCount']] = student_solve_info[['UserId', 'SolvedCount']] # restore UserId and SolvedCount column values.
+student_solve_info_scaled.describe()
+
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2, random_state=1)
+student_solve_info_scaled['AchievementGroup'] = kmeans.fit_predict(student_solve_info_scaled.drop(columns=['UserId', 'SolvedCount'], inplace=False))
+
+# Check which acheivement group has better academic achievement.
+print(student_solve_info_scaled[student_solve_info_scaled['AchievementGroup'] == 0][['CorrectRate', 'MeanConfidence']].mean())
+print(student_solve_info_scaled[student_solve_info_scaled['AchievementGroup'] == 1][['CorrectRate', 'MeanConfidence']].mean())
+# Achievement group 0 has higher value in both CorrectRate and MeanConfidence.
+# Thus, We can say that achievement group 0 is the upper academic achievement group, and group 1 is the lower academic achievement group.
+
+# [Compute mean confidence of each question seperately for two achievement groups] *
+answer_integrated = pd.merge(answer_integrated, student_solve_info_scaled[['UserId', 'AchievementGroup']], 'left', 'UserId')
+answer_integrated['AchievementGroup'].value_counts()
+
+answer_integrated['UpperGroupConfidence'] = answer_integrated.apply(lambda row: row['Confidence'] if row['AchievementGroup'] == 0 else np.NaN, axis='columns')
+answer_integrated['LowerGroupConfidence'] = answer_integrated.apply(lambda row: row['Confidence'] if row['AchievementGroup'] == 1 else np.NaN, axis='columns')
+answer_integrated.describe()
+print(answer_integrated['AchievementGroup'].isnull().sum())
+
+# [Calculate other features for measuring quality, and make up train data] *
+answer_integrated_question_group = answer_integrated.groupby('QuestionId')
+answer_integrated_question_group
+train_data = pd.DataFrame(columns=['IncorrectRate', 'AnswerVariance', 'UpperGroupConfidence', 'LowerGroupConfidence'])
 train_data.index.name = 'QuestionId'
-train_data[['CorrectRate', 'MeanConfidence']] = answer_integrated_group.mean()[['IsCorrect', 'Confidence']]
-train_data['AnswerVariance'] = answer_integrated_group.var()['AnswerValue']
+train_data['AnswerVariance'] = answer_integrated_question_group.var()['AnswerValue']
+train_data['IncorrectRate'] = 1 - answer_integrated_question_group.mean()['IsCorrect']
+# train_data['IncorRateAnsVarInteract'] = train_data['AnswerVariance'] * train_data['IncorrectRate'] # delete later
+train_data[['UpperGroupConfidence', 'LowerGroupConfidence']] = \
+    answer_integrated_question_group.mean()[['UpperGroupConfidence', 'LowerGroupConfidence']]
 
 train_data.isnull().sum()
-print(answer_integrated[answer_integrated['QuestionId']==1].info())
+print(answer_integrated[answer_integrated['QuestionId']==1])
 # There is some questions that none of its answers have confidence info.
-# -> Set mean confidence as their confidence.
+# -> Set mean confidence as their confidence. (for both upper and lower group confidence)
 
 # After GroupBy and Mean operation, the group that only has NaN value will still have NaN value as mean.
 # df = pd.DataFrame({'Id':[1, 1, 2, 2, 3, 3, 4, 4], 'value':[10, 10, 20, 20, 30, np.NAN, np.NAN, np.NAN]})
 # df.groupby('Id').mean()
-train_data['MeanConfidence'] = train_data['MeanConfidence'].fillna(train_data['MeanConfidence'].mean())
+train_data['LowerGroupConfidence'] = train_data['LowerGroupConfidence'].fillna(train_data['LowerGroupConfidence'].mean())
+train_data['UpperGroupConfidence'] = train_data['UpperGroupConfidence'].fillna(train_data['UpperGroupConfidence'].mean())
 train_data.isnull().sum()
 
-# [Preprocessing]
-from sklearn.preprocessing import MinMaxScaler
+# [Preprocessing before PCA] *
 scaler = MinMaxScaler()
-train_data_scaled = scaler.fit(train_data).transform(train_data)
+train_data_scaled = scaler.fit_transform(train_data)
 train_data_scaled = pd.DataFrame(train_data_scaled, index=train_data.index, columns=train_data.columns)
+train_data_scaled.describe()
 
+# [PCA] TODO *
+from sklearn.decomposition import PCA
+train_data_PCA = PCA(n_components=3).fit_transform(train_data_scaled)
+train_data_PCA = pd.DataFrame(data=train_data_PCA, columns=['PC1', 'PC2', 'PC3'])
 
-# [Validation]
+# [Validation] *
 validation_data = pd.read_csv('C:/Users/INHA/Documents/MLTermProject/data/test_data/quality_response_remapped_public.csv', na_values='?')
 template = pd.read_csv('C:/Users/INHA/Documents/MLTermProject/submission/template.csv', na_values='?')
 
@@ -62,77 +120,86 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 fig1 = plt.figure(1, figsize=(4,3), dpi = 200)
-plt.xlabel('Incorrect rate & answer variance coef')
+plt.xlabel('PC1')
 plt.ylabel('Mean expert score')
 
 fig2 = plt.figure(2, figsize=(4,3), dpi = 200)
-plt.xlabel('Confidence coef')
+plt.xlabel('PC2')
 plt.ylabel('Mean expert score')
 
-fig3 = plt.figure(3, figsize=(4,3), dpi = 200)
-ax = fig3.add_subplot(111, projection='3d')
-ax.set_xlabel('Incorrect rate & answer variance coef')
-ax.set_ylabel('Confidence coef')
-ax.set_zlabel('Mean expert score')
+fig2 = plt.figure(3, figsize=(4,3), dpi = 200)
+plt.xlabel('PC3')
+plt.ylabel('Mean expert score')
+
+# fig3 = plt.figure(3, figsize=(4,3), dpi = 200)
+# ax = fig3.add_subplot(111, projection='3d')
+# ax.set_xlabel('Incorrect rate & answer variance coef')
+# ax.set_ylabel('Confidence coef')
+# ax.set_zlabel('Mean expert score')
 
 # Grid Search for finding best coefficients
 best_score = 0.0
 best_question_quality = pd.DataFrame(columns=['QualityMeasure', 'Rank'])
+best_coef_map = {"pc1" : 0.0, "pc2" : 0.0, "pc3" : 0.0}
 
-for incorrect_rate_answer_var_interaction_coef in [0.01, 0.1, 0.3, 0.5, 0.7, 1]:
-    for confidence_coef in [-1.5, -1, -0.85, -0.7, -0.5, -0.3, -0.1, 0.1, 0.3, 0.5, 0.7, 1]:
-
-        # Measure quality
-        question_quality = pd.DataFrame(columns=['QualityMeasure', 'Rank'])
-        question_quality.index.name = 'QuestionId'
-        
-        for index in train_data_scaled.index:
-            question_quality.loc[index] = \
-                incorrect_rate_answer_var_interaction_coef \
-                * (1 - train_data_scaled.at[index, 'CorrectRate']) \
-                * train_data_scaled.at[index, 'AnswerVariance'] \
-                + confidence_coef * train_data_scaled.at[index, 'MeanConfidence']
-        
-        # Calculate quality rank
-        question_quality['Rank'] = question_quality['QualityMeasure'].rank(method='first', ascending=False)        
-        question_quality.describe()
-        
-        question_quality_compare = []
-        for index in validation_data.index:
-            left_question = validation_data.at[index, 'left']
-            right_question = validation_data.at[index, 'right']
-            question_quality_compare.append(1 if question_quality['Rank'][left_question] < question_quality['Rank'][right_question] else 2)
-        
-        validation_scores = pd.Series([0.0, 0.0, 0.0, 0.0, 0.0])
-        for index in validation_data.index:
-            if question_quality_compare[index] == validation_data['T1_ALR'][index]:
-                validation_scores[0] += 1
-            if question_quality_compare[index] == validation_data['T2_CL'][index]:
-                validation_scores[1] += 1
-            if question_quality_compare[index] == validation_data['T3_GF'][index]:
-                validation_scores[2] += 1
-            if question_quality_compare[index] == validation_data['T4_MQ'][index]:
-                validation_scores[3] += 1
-            if question_quality_compare[index] == validation_data['T5_NS'][index]:
-                validation_scores[4] += 1
-        
-        for expert in range(5):
-            validation_scores[expert] = validation_scores[expert] / len(validation_data)
-                        
-        mean_score = validation_scores.mean()
-        if mean_score > best_score:
-            best_score = mean_score
-            best_question_quality = question_quality
+for pc1_coef in [-0.30, -0.20, -0.1, 0.0, 0.1, 0.20, 0.30]:
+    for pc2_coef in [-0.30, -0.20, -0.1, 0.0, 0.1, 0.20, 0.30]:
+        for pc3_coef in [-0.30, -0.20, -0.1, 0.0, 0.1, 0.20, 0.30]:
+    
+            # Measure quality
+            question_quality = pd.DataFrame(columns=['QualityMeasure', 'Rank'])
+            question_quality.index.name = 'QuestionId'
             
-        #  Plot the mean score with current coefficient values
-        plt.figure(1)
-        plt.scatter(incorrect_rate_answer_var_interaction_coef, mean_score)
-        plt.figure(2)
-        plt.scatter(confidence_coef, mean_score)
-        plt.figure(3)
-        ax.scatter(incorrect_rate_answer_var_interaction_coef, confidence_coef, mean_score)
-        
+            for index in train_data_PCA.index:
+                question_quality.loc[index] = \
+                    pc1_coef * train_data_PCA.at[index, 'PC1'] \
+                  + pc2_coef * train_data_PCA.at[index, 'PC2'] \
+                  + pc3_coef * train_data_PCA.at[index, 'PC3']
+            
+            # Calculate quality rank
+            question_quality['Rank'] = question_quality['QualityMeasure'].rank(method='first', ascending=False)        
+            question_quality.describe()
+            
+            question_quality_compare = []
+            for index in validation_data.index:
+                left_question = validation_data.at[index, 'left']
+                right_question = validation_data.at[index, 'right']
+                question_quality_compare.append(1 if question_quality['Rank'][left_question] < question_quality['Rank'][right_question] else 2)
+            
+            validation_scores = pd.Series([0.0, 0.0, 0.0, 0.0, 0.0])
+            for index in validation_data.index:
+                if question_quality_compare[index] == validation_data['T1_ALR'][index]:
+                    validation_scores[0] += 1
+                if question_quality_compare[index] == validation_data['T2_CL'][index]:
+                    validation_scores[1] += 1
+                if question_quality_compare[index] == validation_data['T3_GF'][index]:
+                    validation_scores[2] += 1
+                if question_quality_compare[index] == validation_data['T4_MQ'][index]:
+                    validation_scores[3] += 1
+                if question_quality_compare[index] == validation_data['T5_NS'][index]:
+                    validation_scores[4] += 1
+            
+            for expert in range(5):
+                validation_scores[expert] = validation_scores[expert] / len(validation_data)
+                            
+            mean_score = validation_scores.mean()
+            if mean_score > best_score:
+                best_score = mean_score
+                best_question_quality = question_quality
+                best_coef_map['pc1'] = pc1_coef
+                best_coef_map['pc2'] = pc2_coef                
+                best_coef_map['pc3'] = pc3_coef
+                
+            #  Plot the mean score with current coefficient values
+            plt.figure(1)
+            plt.scatter(pc1_coef, mean_score)
+            plt.figure(2)
+            plt.scatter(pc2_coef, mean_score)
+            plt.figure(3)
+            plt.scatter(pc3_coef, mean_score)
+            
 print("Max Validation Mean Score: {0}".format(best_score))
+print("Best Coefficient: {0}".format(best_coef_map))
 
 # Write rank in csv file
 best_question_quality = best_question_quality.astype(dtype={'QualityMeasure':'float64', 'Rank':'int64'})
